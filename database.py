01c9fca91999513054a15bf9de8fc7b2ff161e8c
jay takalgavankar <jaytakalgavankar@gmail.com>
2025-06-14 13:42:14 +0530

    fix: added python-multipart to requirements

--- database.py
+++ database.py
@@ -1,11 +1,11 @@
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "postgresql+asyncpg://postgres:jay%40123@localhost/postgres"
+DATABASE_URL = "postgresql+asyncpg://postgress:dqlFbiCCWmCycyjPLSN1hl2wIc3bNNdB@dpg-d16ih195pdvs73fc2g1g-a/fastapi_db_2f7f"
 
 Base = declarative_base()
 engine = create_async_engine(DATABASE_URL, echo=True)
-AsyncSessionLocal = sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
+AsyncSessionLocal = sessionmaker(bind=engine, class_=AsyncSession, expire_on_commit=False)
 
 async def get_db():
     async with AsyncSessionLocal() as session:
